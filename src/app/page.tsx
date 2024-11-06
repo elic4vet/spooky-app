b680435c1103e58010285f5deb395810c9d37eb9
Elisabeth <elise.doisneau@gmail.com>
2024-11-06 20:12:33 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import me from "../assets/me.png"
+import { Link } from "lucide-react";
 
 export default function Home() {
   return (
@@ -28,13 +29,11 @@ export default function Home() {
             Download my CV
           </a>
 
-          <a
+          <Link
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="#"
-            target="_blank"
-            rel="noopener noreferrer">
+            href="./components/About/aboutme.tsx">
             About me
-          </a>
+          </Link>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
