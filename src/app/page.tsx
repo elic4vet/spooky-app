5e0ce316491f7970b174c0286838d4bce65274fa
Elisabeth <elise.doisneau@gmail.com>
2024-11-06 20:06:54 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="#"
+            href="https://docs.google.com/document/d/1AXzabGqPFsaHUKLgsPYk4ExFrILvhU4CDIgGqHWLa48/edit?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
